1c513c652d1d0968b58aed30cf1fe64077f49bf0
Ramnath Sharma <ramsharma.axel@gmail.com>
2020-08-21 10:53:37 +0530

    login added

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,20 +2,16 @@ from django.forms import ModelForm
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from .models import Customer
 
 
-class CreateUserForm(UserCreationForm):
-	class Meta:
-		model = User
-		fields = ['username', 'email', 'password1', 'password2']
-
-class UserRegistrationForm(UserCreatioForm):
+class UserRegistrationForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = [
 			'email',
-			'phone',
-			'password1'
+			'password1',
 			'password2'
 		]
 
@@ -23,12 +19,24 @@ class CustomerForm(forms.ModelForm):
 	class Meta:
 		model = Customer
 		fields = [
+
 			'name',
 			'gender',
+			'phone'
 
 		]
-
 class UserLoginForm(forms.Form):
-	account_no = forms.IntegerField(label="Account Number")
-	password = forms.CharField(widget = forms.PasswordInput)
-	
+	account_no = forms.CharField()
+	password = forms.CharField(widget=forms.PasswordInput)
+
+	def clean(self, *args, **kwargs):
+		username = self.cleaned_data.get('account_no')
+		password = self.cleaned_data.get('password')
+
+		if username and password:
+			user = authenticate(username=username, password=password)
+			if not user:
+				raise forms.ValidationError('this user does not exist')
+			if not user.check_password(password):
+				raise forms.ValidationError('Incorrect Password')
+			return super(UserLoginForm, self).clean(*args, **kwargs)
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from .forms import UserLoginForm
+from django.contrib import messages
+from .models import Customer
 # Create your views here.
 def register_view(request):
 	return HttpResponse('Register')
 
 def login_view(request):
 	if request.user.is_authenticated:
-		return redirect('home')
+		return redirect('transactions:home')
 	else:
 		form = UserLoginForm(request.POST)
-		
-
+		if form.is_valid():
+			username = form.cleaned_data.get('account_no')
+			password = form.cleaned_data.get('password')
+			user = authenticate(request, username=username, password=password)
+			login(request, user)
+			customer = Customer.objects.get(account_no = int(username))
+			messages.success(request, 'Welcome, {}!' .format(customer.name))
+			return redirect('transactions:home')
+		context = {
+			'form':form,
+			'title':'Login'
+		}
+		return render(request, "accounts/form.html", context)
 
 
 def logout_view(request):
@@ -19,4 +33,4 @@ def logout_view(request):
         return redirect('accounts:login')
     else:
         logout(request)
-        return redirect('transactions:home')	
+        return redirect('transactions:home')
